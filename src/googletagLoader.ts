83b20cd07f0f2ca1584202cc735a7353339acc9f
Joe Flateau <joe@joeflateau.net>
2021-07-30 15:48:49 -0400

    waiting for script to load isn't enough, also need to wait for the googletag.cmd callback

--- src/googletagLoader.ts
+++ src/googletagLoader.ts
@@ -1,7 +1,19 @@
 import PLazy from "p-lazy";
 import { loadScript } from "./loadScript";
 
+declare global {
+  interface Window {
+    googletag: googletag.Googletag;
+  }
+}
+
 export const googletagLoader = PLazy.from(async () => {
   await loadScript("https://securepubads.g.doubleclick.net/tag/js/gpt.js");
+  window.googletag = window.googletag || { cmd: [] };
+  await new Promise<void>((resolve) => {
+    window.googletag.cmd.push(() => {
+      resolve();
+    });
+  });
   return googletag;
 });
--- src/loadScript.ts
+++ src/loadScript.ts
@@ -1,6 +1,7 @@
 export function loadScript(src: string) {
   return new Promise<void>((resolve, reject) => {
     const scriptEl = document.createElement("script");
+    scriptEl.async = true;
     scriptEl.onload = () => resolve();
     scriptEl.onerror = () => reject();
     scriptEl.src = src;
